6195995e75d5eb6a42a5cb973ad4fd2dc5c752bc
bentarman <btarman@mymail.mines.edu>
2018-09-09 19:02:21 -0600

    jibun wo

--- echo_s.c
+++ echo_s.c
@@ -1,31 +1,61 @@
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <time.h>
 
 #define LISTEN_BACKLOG 1
-#define PORT 9872
+
+#define KRED   "\x1b[31m"
+#define KYEL   "\x1b[33m"
+#define KGREEN "\x1b[32m"
+#define KBLUE  "\x1b[34m"
+#define KRESET "\x1b[0m"
+	
+
+void debug(char* format, ...) 
+{
+		va_list args;
+		char buf[128];
+		sprintf(buf, format, args);
+		printf(buf);
+}
 
 int processConnection(int connfd) 
 {
 
-	char buffer[1024]; //make 1k bits the max i suppose
-
+	char buffer[256]; //make 1k bits the max i suppose
 	ssize_t read_size;
 
-	while ( (read_size = recv(connfd, buffer, 1024, 0)) > 0)
+	//clear buffer (prevents overflows)
+	bzero(buffer,256);
+debug(KYEL "calling read..." KRESET "\n");
+	while ( (read_size = recv(connfd, buffer, 256, 0)) > 0)
 	{
-		printf("reading %ld bytes\n", read_size);
+		//buffer[strcspn(buffer, "\n\r")] = '\0'; //clears newline from buffer
+
+		debug("Received %ld bytes, containing the string \"%s\"\n", read_size, buffer);
 
 		if (strncmp("CLOSE", buffer, 5) == 0)
 		{
-			puts("lets close this shiz");
+			debug(KBLUE "Close command found" KRESET "\n");
 			return 0;
 		}
+		
+		if (strncmp("QUIT", buffer, 4) == 0) 
+		{
+			debug(KBLUE "QUIT command found" KRESET "\n");
+			return 1;
+		}
+
+
+		debug(KYEL "calling write..." KRESET "\n");
 		write(connfd, buffer, strlen(buffer));
+		write(connfd, "\n", strlen("\n"));
+		debug(KGREEN "Wrote %ld back to client" KRESET "\n", read_size);
 
 	}
 
@@ -33,67 +63,103 @@ int processConnection(int connfd)
 	if (read_size == 0) 
 	{
 		puts("client disconnected");
-		printf("read failed %s\n", strerror(errno));
+		debug("read failed %s\n", strerror(errno));
 		return -1;
 	}
 
-	printf("data acknowledged received %ld bytes\n", read_size);
+	debug("data acknowledged received %ld bytes\n", read_size);
 
 	return 0;
 }
 
-int main() 
+int main(int argc, char **argv) 
 {
+  int opt = 0;
+	int isVerbose = 0;
 
+	// check if verbose mode enabled
+  while ((opt = getopt(argc,argv,"v")) != -1) {
+    
+    switch (opt) {
+    case 'v':
+			isVerbose = 1;
+      break;
+    case ':':
+    case '?':
+    default:
+			debug("usage: %s -v\n", argv[0]);
+      exit(-1);
+    }
+  }
+
+
+	// start the actual socket programming
+	
 	int lfd = -1;
-
 	struct sockaddr_in my_addr;
+	
+	debug( KYEL "creating socket...." KRESET "\n");
 
 	if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 	{
-		//handle_error("socket");
-		printf("Failed to create listening socket");
+		debug( KRED "Failed to create listening socket" KRESET "\n");
+		exit(-1);
 	}
 
-	printf("created socket\n");
+	debug( KGREEN "created socket success (assigned file descriptor %d)" KRESET "\n", lfd);
+
+	// assign random port number
+	srand (time(NULL));
+	int port = (rand() % (10000 - 1 + 1)) + 1; 
+	char port_str[20];
+	sprintf(port_str, "Using port %d", port);
+	puts(port_str);
 
 	//clear whole structure
 	memset(&my_addr, 0, sizeof(struct sockaddr_in));
-
 	my_addr.sin_family = PF_INET;
-
 	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	my_addr.sin_port = htons(PORT);
+	my_addr.sin_port = htons(port);
 
+
+	debug( KYEL "binding socket...." KRESET "\n");
 
 	if (bind(lfd, (struct sockaddr *) &my_addr, 
 				sizeof(struct sockaddr_in)) == -1) 
 	{
-		printf("bind failed %s\n", strerror(errno));
+		debug(KRED "bind socket failed %s" KRESET "\n", strerror(errno));
 	}
+
+	debug( KGREEN "bind socket success" KRESET "\n");
 	
-	printf("bind socket\n");
+	debug( KYEL "listen on socket..." KRESET "\n");
 
 	if (listen(lfd, LISTEN_BACKLOG) == -1) 
 	{
-		printf("listen failed %s\n", strerror(errno));
+		debug( KRED "listen failed %s" KRESET "\n", strerror(errno));
 	}
 	
-	printf("listening on scoekt...\n");
+	debug( KGREEN "socket now listening..." KRESET "\n");
 
 
-
+	int quitProgram = 0;
+	while (!quitProgram) 
+	{
 		int connfd = -1;
+		debug( KYEL "accepting socket...." KRESET "\n");
 		if ((connfd = accept(lfd, (struct sockaddr *) NULL, NULL)) < 0) 
 		{
-			printf("acceptfailed %s\n", strerror(errno));
+			debug( KRED "acceptfailed %s" KRESET "\n", strerror(errno));
 		}
-
-		int ret = processConnection(connfd);
-
+		
+		debug( KGREEN "socket accepted (connected on %d)" KRESET "\n", connfd);
+		quitProgram = processConnection(connfd);
 		close(connfd);
+		debug( KBLUE "CONNECTION CLOSED" KRESET "\n");
+	}
+		
+	debug( KBLUE "PROGRAM QUITTED" KRESET "\n");
 
-		printf("accept success\n");
 
 	return 0;
 }
